5e65a1faaf4d2d14ab419e27980db64cb25a7a36
Christian Whitesides <whitesidescr@gmail.com>
2016-07-11 17:02:03 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :students
+  # ** 4.6 Restricting the Routes Created **
+  # By default, Rails creates routes for the seven default actions (index, show, new, create, edit, update, and destroy) 
+  # for every RESTful route in your application. You can use the :only and :except options to fine-tune this behavior. 
+  # The :only option tells Rails to create only the specified routes.
+  resources :students, only: :index
 end
