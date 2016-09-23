1adf5722997da45f76fd5fcdfc498d25e465357c
Traci <Tracir@gmail.com>
2016-09-23 15:50:21 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+student = Student.create(:first_name => "Traci", :last_name => "Thompson")
+student = Student.create(:first_name => "Bryan", :last_name => "Thompson")
