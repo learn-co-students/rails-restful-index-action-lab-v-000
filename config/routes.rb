bcc0a8d0466fe7925f1e1df134f3f0b86b140320
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-29 12:10:57 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  Rails.application.routes.draw do
+    resources :students, only: :index
+  end 
+
 end
