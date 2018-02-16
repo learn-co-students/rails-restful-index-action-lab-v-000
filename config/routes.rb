7abcd148dfe9ba39c70f5503dae69ec0eb08ca63
EC2 Default User <ec2-user@ip-172-31-8-108.ec2.internal>
2018-02-16 22:08:18 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    
+  get '/students', to: 'students#index.html'
+#   get '/index.html', to: 'students#index.html'
+    # resources :students, only: :index
+   
+end 
   
-end
+
