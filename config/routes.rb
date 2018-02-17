eeb080e833a20453f448a49e0bf896cd80996965
lsivak <https://github.com/lsivak>
2018-02-16 21:19:22 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-    
+
   get '/students', to: 'students#index.html'
 #   get '/index.html', to: 'students#index.html'
-    # resources :students, only: :index
-   
-end 
-  
+     resources :students, only: :index
 
+end
