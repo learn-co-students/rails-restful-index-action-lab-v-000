25c067fa0dc332ae20b4f0e4e7bca5696848f33b
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-30 10:33:22 -0500

    passing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+ 
+  get '/students', to: 'students#index', as: 'students'
+
+  get '/student/new', to: 'students#new'
+
+  get 'students/:id', to: 'students#show', as: 'student'
+
 end
