e94cccd3f0bd7166a2edcfee81ba489eaee12676
AlexFriedland <friedlandaj@gmail.com>
2018-05-17 16:11:32 +0200

    Done.

--- spec/features/student_index_spec.rb
+++ spec/features/student_index_spec.rb
@@ -14,7 +14,7 @@ describe 'Multiple students are shown' do
     Student.create!(first_name: "Lindsey", last_name: "Stirling")
 
     visit "/students"
-
-    expect(page).to have_content(/(?=.*Daenerys)(?=.*Lindsey).*/)
+    #save_and_open_page
+    expect(page).to have_content(/Daenerys|Lindsey/)
   end
 end
