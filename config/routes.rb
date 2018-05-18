6686bb0b40a959bf4220829d165319284f8f3cd9
Carol Greene <carolgreene3@gmail.com>
2018-05-18 00:26:04 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'students' => 'students#index'
+  get 'students' => 'students#index'      #format from Avi's video
 
 end
+
+
+#solution has as follows:                    #format from edgeguides.rubyonrails
+#Rails.application.routes.draw do
+  #resources :students, only: :index
+#end
