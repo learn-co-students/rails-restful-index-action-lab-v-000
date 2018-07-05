bef1819a057330b99e6f5add7b183b9456cfd081
Karl Amoguis <ktamoguis@gmail.com>
2018-07-05 09:20:47 +0000

    Done.

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,9 +1,4 @@
 class StudentController < ApplicationController
 
-  index '/students' do
-    @students = Student.all
-
-    erb :'/index'
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+
+  #get 'hello_world', to: 'static#hello_world'
+  get 'students', to: 'student#index'
+
+
 end
