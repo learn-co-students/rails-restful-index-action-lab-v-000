01ba36bf9a641f948435eef118a2d24f03bebf80
epic0448 <samuelogundiran@Yahoo.com>
2018-07-20 21:41:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+          
+    get '/students', to: 'students#index'
+        
+      #url -> '/student'                 
+       #student - controller
+      #index - action(in controller.)
+
+# resources :students
+
 end
+ 
