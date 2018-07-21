095898f0e8616d68a58824156d32d2d991556ebc
epic0448 <samuelogundiran@Yahoo.com>
2018-07-20 21:42:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
           
     get '/students', to: 'students#index'
+         or
+
+         # resources :students
+
         
       #url -> '/student'                 
        #student - controller
       #index - action(in controller.)
+      # implicit simply means, if you dont give it a direction... It gonna look for one based on its controller 
+      # the methods in that controller.
 
-# resources :students
 
 end
  
