e492cf62d545d99bce86a0935e55fa45a60c7da2
Kachel Wittig <kjwittig@gmail.com>
2018-08-09 16:05:11 -0500

    donezo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  # 'students' is the path in the URL where the route is mapped to
+  # 'student#index' passed through the `students` controller through the `index` action
+  get 'students', to: 'students#index'
+
 end
