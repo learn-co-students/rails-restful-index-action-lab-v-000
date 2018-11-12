019ea2b0da090107dfab3102635bcfec1302bdb7
Judy McGoldrick <jjeanius@verizon.net>
2018-11-12 20:01:44 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,7 @@
 class StudentsController < ApplicationController
 
-  #get "students", to: 'students#index'
-
  def index
-   Student.creat(first_name, last_name)
-   render :plain => @students = Student.all
- end
+   @students = Student.all
 
+ end
 end
