8f4b44f3d7d8c5150526a9bd7b98200e738ef418
Harley Rayner <harleymteden@gmail.com>
2019-01-08 05:36:01 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-  end 
+    @students = Student.all
+  end
+
+  def to_s
+    self.first_name.capitalize + " " + self.last_name.capitalize
+  end
 
 end
