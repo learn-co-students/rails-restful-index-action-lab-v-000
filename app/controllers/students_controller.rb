a2d08c1c75f36d794ca52bad4f950cb8b57a68c4
Sushi Chanrai <sushi@pathworks.co>
2019-01-14 12:01:36 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,8 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
-    render 'students/index'
+    #render 'students/index' - if you store within the folder students then implicit works
+
   end
 
 end
