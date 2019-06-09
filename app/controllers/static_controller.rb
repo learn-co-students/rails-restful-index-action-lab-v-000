6b7a09467ae032d5daf826d96c06495766e17ff9
github username <github email address>
2019-06-09 17:07:29 -0400

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 class StaticController < ApplicationController
+    #controller inheriting from ApplicationController
     def students 
+        #def action
         render "index"
+        #render html.erb page
     end
 end
